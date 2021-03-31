078d593bf7f1026f8bb6c2ce2f92edbe179f55e3
m.quemeneurleblois <marie.quemeneur-leblois@woonoz.com>
2021-03-31 10:12:10 +0200

    from Spring Boot tutorial
    https://www.baeldung.com/spring-boot-start

--- src/main/java/lebloism/pmt/shorturl/ShorturlApplication.java
+++ src/main/java/lebloism/pmt/shorturl/ShorturlApplication.java
@@ -2,8 +2,13 @@ package lebloism.pmt.shorturl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EnableJpaRepositories("lebloism.pmt.shorturl.dao")
+@EntityScan("lebloism.pmt.shorturl.model")
+@SpringBootApplication(scanBasePackages = "lebloism.pmt.shorturl")
 public class ShorturlApplication {
 
 	public static void main(String[] args) {
